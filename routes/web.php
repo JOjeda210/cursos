e48ca4fe4b774cb3bb99ffc0abbc648993e636d3
Jesús Ojeda <ojedajesus232@gmail.com>
2025-11-05 22:48:18 -0500

    feat(auth): endpoints para auth

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\HealthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+// heathchek de db:
+Route::get('/helth', [HealthController::class, 'check']); 
 
 
+// Endpoints de auth
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
