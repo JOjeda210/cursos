46e55f3f12b655ecde501ae59da727790295a0df
AmaVG <202400325@upqroo.edu.mx>
2025-11-09 16:17:33 -0500

    Se añadieron las migraciones y servisio para el curso

---
+++ database/migrations/2025_11_09_192610_create_modulos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement("
+            create table modulos (
+                id_modulo int primary key auto_increment,
+                id_curso int not null,
+                titulo varchar(150) not null,
+                orden int not null,
+                foreign key (id_curso) references cursos(id_curso)
+            );
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement("drop table if exists modulos");
+    }
+};
